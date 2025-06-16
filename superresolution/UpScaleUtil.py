c53ccbf166ca7d68ec9e68ec6c11d470e72467d1
Voldie <voldiekami@gmail.com>
2025-06-16 17:31:43 +0200

    Wip improving video and upscale logic

--- superresolution/UpScaleUtil.py
+++ superresolution/UpScaleUtil.py
@@ -8,6 +8,8 @@ import traceback
 from logging import Logger
 from multiprocessing import Pool
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
+
 import numpy as np
 import tensorflow as tf
 from PIL import Image
@@ -15,6 +17,8 @@ from skimage.color import rgb2lab
 
 from core.common import setup_tensorflow_strategy
 from util.util import upscale_image_func
+
+
 
 sr_logger: Logger = logging.getLogger('SuperResolution Upscale Program')
 #
@@ -25,87 +29,94 @@ console_handler.setFormatter(logging.Formatter(log_format))
 sr_logger.addHandler(console_handler)
 
 
-def upscale_logic(input_im: Image, image_input_size: tuple, image_scale: tuple, batch_size, color_space, upscale_model):
-	# Open File and Convert to RGB Color Space.
-	input_im: Image = input_im.convert('RGB')
-	width_scale, height_scale = image_scale
-	input_width, input_height, _ = image_input_size
+# TODO: relocate
+class UpScale:
 
-	#
-	upscale_new_size: tuple = (
-		int(input_im.size[0] * width_scale), int(input_im.size[1] * height_scale))
-	sr_logger.info("Upscale Size " + str(upscale_new_size))
+	def __init__(self, model_path, color_space):
+		self.upscale_model = tf.keras.models.load_model(filepath=model_path, compile=False)
+		self.color_space = color_space
 
-	# New Upscale Size.
-	upscale_image = Image.new("RGB", upscale_new_size, (0, 0, 0))
-
-	#
-	nr_width_block: int = math.ceil(
-		float(input_im.width) / float(input_width))
-	nr_height_block: int = math.ceil(
-		float(input_im.height) / float(input_height))
-
-	sr_logger.debug(str.format(
-		"Number of tiles: {0}:{1}", nr_width_block, nr_height_block))
-
-	# Construct all crops.
-	image_crop_list: list = []
-	for x in range(0, nr_width_block):
-		for y in range(0, nr_height_block):
-			# Compute subset view.
-			left = x * input_width
-			top = y * input_height
-			right = (x + 1) * input_width
-			bottom = (y + 1) * input_height
-			image_crop_list.append((left, top, right, bottom))
-
-	# Compute number of cropped batches.
-	nr_cropped_batches: int = int(
-		math.ceil(len(image_crop_list) / batch_size))
-
-	#
-	for nth_batch in range(0, nr_cropped_batches):
-		cropped_batch = image_crop_list[nth_batch *
-		                                batch_size:(nth_batch + 1) * batch_size]
-
-		crop_batch = []
-		for crop in cropped_batch:
-			cropped_sub_input_image = input_im.crop(crop)
-			crop_batch.append(np.array(cropped_sub_input_image))
-
-		normalized_subimage_color = (np.array(crop_batch) * (1.0 / 255.0)).astype(
-			dtype='float32')
-
-		# TODO fix color space conversation, use function.
-		if color_space == 'lab':
-			cropped_sub_input_image = rgb2lab(
-				normalized_subimage_color) * (1.0 / 128.0)
-		elif color_space == 'rgb':
-			cropped_sub_input_image = (
-				                          normalized_subimage_color * 2) - 1
-
-		# Upscale.
-		upscale_raw_result = upscale_image_func(upscale_model, cropped_sub_input_image,
-		                                        color_space=color_space)
+	def upscale_image(self, input_im: Image, image_input_size: tuple, image_scale: tuple, batch_size: int) -> (Image, tuple): # type: ignore
+		# Open File and Convert to RGB Color Space.
+		input_im: Image = input_im.convert('RGB')
+		width_scale, height_scale = image_scale
+		input_width, input_height, _ = image_input_size
 
 		#
-		for index, (crop, upscale) in enumerate(zip(cropped_batch, upscale_raw_result)):
-			output_left = int(crop[0] * width_scale)
-			output_top = int(crop[1] * width_scale)
-			output_right = int(crop[2] * width_scale)
-			output_bottom = int(crop[3] * width_scale)
+		upscale_new_size: tuple = (
+			int(input_im.size[0] * width_scale), int(input_im.size[1] * height_scale))
+		sr_logger.info("Upscale Size " + str(upscale_new_size))
 
-			upscale_image.paste(
-				upscale, (output_left, output_top, output_right, output_bottom))
+		# New Upscale Size.
+		upscale_image = Image.new("RGB", upscale_new_size, (0, 0, 0))
 
-	# Offload final crop and save to separate thread.
-	final_cropped_size = (
-		0, 0, upscale_new_size[0], upscale_new_size[1])
-	return upscale_image, final_cropped_size
+		#
+		nr_width_block: int = math.ceil(
+			float(input_im.width) / float(input_width))
+		nr_height_block: int = math.ceil(
+			float(input_im.height) / float(input_height))
+
+		sr_logger.debug(str.format(
+			"Number of tiles: {0}:{1}", nr_width_block, nr_height_block))
+
+		# Construct all crops.
+		image_crop_list: list = []
+		for x in range(0, nr_width_block):
+			for y in range(0, nr_height_block):
+				# Compute subset view.
+				left = x * input_width
+				top = y * input_height
+				right = (x + 1) * input_width
+				bottom = (y + 1) * input_height
+				image_crop_list.append((left, top, right, bottom))
+
+		# Compute number of cropped batches.
+		nr_cropped_batches: int = int(
+			math.ceil(len(image_crop_list) / batch_size))
+
+		#
+		for nth_batch in range(0, nr_cropped_batches):
+			cropped_batch = image_crop_list[nth_batch *
+			                                batch_size:(nth_batch + 1) * batch_size]
+
+			crop_batch = []
+			for crop in cropped_batch:
+				cropped_sub_input_image = input_im.crop(crop)
+				crop_batch.append(np.array(cropped_sub_input_image))
+
+			normalized_subimage_color = (np.array(crop_batch) * (1.0 / 255.0)).astype(
+				dtype='float32')
+
+			# TODO fix color space conversation, use function.
+			if self.color_space == 'lab':
+				cropped_sub_input_image = rgb2lab(
+					normalized_subimage_color) * (1.0 / 128.0)
+			elif self.color_space == 'rgb':
+				cropped_sub_input_image = (
+					                          normalized_subimage_color * 2) - 1
+
+			# Upscale.
+			upscale_raw_result = upscale_image_func(self.upscale_model, cropped_sub_input_image,
+			                                        color_space=self.color_space)
+
+			#
+			for index, (crop, upscale) in enumerate(zip(cropped_batch, upscale_raw_result)):
+				output_left = int(crop[0] * width_scale)
+				output_top = int(crop[1] * width_scale)
+				output_right = int(crop[2] * width_scale)
+				output_bottom = int(crop[3] * width_scale)
+
+				upscale_image.paste(
+					upscale, (output_left, output_top, output_right, output_bottom))
+
+		# Offload final crop and save to separate thread.
+		final_cropped_size = (
+			0, 0, upscale_new_size[0], upscale_new_size[1])
+		return upscale_image, final_cropped_size
 
 
 def save_result_2_file(argument):
-	upscale_image, new_cropped_size, full_output_path = argument
+	frame_index, upscale_image, new_cropped_size, full_output_path = argument
 
 	# Crop image final size image.
 	upscale_image = upscale_image.crop(new_cropped_size)
@@ -119,7 +130,7 @@ def save_result_2_file(argument):
 
 def create_parser() -> argparse.ArgumentParser:
 	parser = argparse.ArgumentParser(
-		description='UpScale',epilog='')
+		description='UpScale', epilog='')
 
 	#
 	parser.add_argument('--save-output', dest='save_path',
@@ -180,6 +191,11 @@ def super_resolution_upscale(argv, **kwargs):
 	if args.debug:
 		sr_logger.setLevel(logging.DEBUG)
 
+
+	async_finished_callback = save_result_2_file # Default output file path.
+	if "upscale_callback" in kwargs.keys():  # Optionally
+		async_finished_callback = kwargs["upscale_callback"]
+
 	# Allow to use multiple GPU
 	strategy = setup_tensorflow_strategy(args=args)
 	sr_logger.info('Number of devices: {0}'.format(
@@ -192,14 +208,14 @@ def super_resolution_upscale(argv, **kwargs):
 			os.mkdir(output_path)
 
 		input_filepaths = args.input_files
-		if "np" in kwargs.keys():	# Optionally
+		if "np" in kwargs.keys():  # Optionally
 			input_filepaths = kwargs["np"]
 
 		# TODO improved extraction of filepaths.
-		if isinstance(input_filepaths, (np.ndarray)): #  Load from Memory.
+		if isinstance(input_filepaths, (np.ndarray)):  # Load from Memory.
 			sr_logger.info("Input files is numpy array")
 
-		else: # Load from Files
+		else:  # Load from Files
 			# Check if Directory => search for file inside
 			if os.path.isdir(input_filepaths):
 				sr_logger.info("Directory Path: " + str(input_filepaths))
@@ -207,7 +223,7 @@ def super_resolution_upscale(argv, **kwargs):
 				base_bath = input_filepaths
 				input_filepaths: list = [os.path.join(
 					base_bath, path) for path in all_files]
-				#TODO: sort by filename
+			# TODO: sort by filename
 			else:  # Convert to list
 				sr_logger.info("File Path: " + str(input_filepaths))
 				input_filepaths: list = [input_filepaths]
@@ -216,20 +232,22 @@ def super_resolution_upscale(argv, **kwargs):
 
 		sr_logger.info("Number of files/images {0}".format(len(input_filepaths)))
 
-		upscale_model = tf.keras.models.load_model(
-			filepath=args.model_filepath, compile=False)
-
-		# Optionally, load specific weight.
-		if args.model_weight_path:
-			upscale_model.load_weights(filepath=args.model_weight_path)
+		# upscale_model = tf.keras.models.load_model(
+		#	filepath=args.model_filepath, compile=False)
 
 		color_space: str = args.color_space
 
-		upscale_model.summary()
+		upscale_obj = UpScale(model_path=args.model_filepath, color_space=color_space)
+
+		# Optionally, load specific weight.
+		if args.model_weight_path:
+			upscale_obj.upscale_model.load_weights(filepath=args.model_weight_path)
+
+		upscale_obj.upscale_model.summary()
 
 		#
-		image_input_shape: tuple = upscale_model.input_shape[1:]
-		image_output_shape: tuple = upscale_model.output_shape[1:]
+		image_input_shape: tuple = upscale_obj.upscale_model.input_shape[1:]
+		image_output_shape: tuple = upscale_obj.upscale_model.output_shape[1:]
 
 		#
 		input_width, input_height, input_channels = image_input_shape
@@ -242,7 +260,7 @@ def super_resolution_upscale(argv, **kwargs):
 		               " UpscaleY: " + str(height_scale))
 
 		# Create a pool of task scheduler.
-		pool = Pool(processes=16)
+		pool = Pool(processes=1)
 
 		for index, input_file_path in enumerate(input_filepaths):
 
@@ -263,17 +281,19 @@ def super_resolution_upscale(argv, **kwargs):
 					output_path, "{0}.png".format(index))
 				input_im = Image.fromarray(input_file_path)
 
-
-			upscale_image, final_cropped_size = upscale_logic(input_im=input_im, image_input_size=image_input_shape,
-			                                                  image_scale=(
-				                                                  width_scale, height_scale), batch_size=batch_size,
-			                                                  color_space=color_space, upscale_model=upscale_model)
+			upscale_image, final_cropped_size = upscale_obj.upscale_image(input_im=input_im,
+			                                                              image_input_size=image_input_shape,
+			                                                              image_scale=(
+				                                                              width_scale, height_scale),
+			                                                              batch_size=batch_size)
 
 			# Saving async to prevent the main thread stalling - to allow dispatching of inference on the GPU faster.
 			sr_logger.debug(str.format("Saving {0}", full_output_path))
-
-			pool.apply_async(save_result_2_file, [
-				(upscale_image, final_cropped_size, full_output_path)])
+			
+			if async_finished_callback:
+				async_finished_callback([index, upscale_image, final_cropped_size, full_output_path])
+				#pool.apply_async(lambda x: async_finished_callback(x), [
+				#	(index, upscale_image, final_cropped_size, full_output_path)])
 		# Close and wait intill all tasks has been finished.
 		pool.close()
 		pool.join()
--- superresolution/UpScaleVideo.py
+++ superresolution/UpScaleVideo.py
@@ -1,6 +1,10 @@
 # !/usr/bin/env python3
-import logging
+
 import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
+
+import logging
 import shutil
 import subprocess
 import sys
@@ -9,28 +13,30 @@ import ffmpeg
 import numpy as np
 from UpScaleUtil import create_parser, super_resolution_upscale, sr_logger
 import tempfile
+from PIL import Image
 from shutil import which
-
 
 def super_resolution_upscale_video(argv):
 	parser = create_parser()
 
-	parser.add_argument('--cache-disk', type=str,
-					default='libx256', dest='cache_disk')
+	parser.add_argument('--cache-disk', type=bool,
+	                    default=False, dest='cache_disk')
 	parser.add_argument('--codec', type=str,
-	                    default='libx256', dest='codec')
+	                    default='av1', dest='codec')
+	parser.add_argument('--frames', type=int,
+	                    default=0, dest='frames')	
 
-	args = parser.parse_args(args=argv)
+	parsed_args = parser.parse_args(args=argv)
 
-	# 
+	#
 	sr_logger.setLevel(logging.INFO)
-	if args.debug:
+	if parsed_args.debug:
 		sr_logger.setLevel(logging.DEBUG)
-		
+
 	sr_logger.info("Parsed Arguments")
 
 	# Handle input file path
-	input_filepaths = args.input_files
+	input_filepaths = parsed_args.input_files
 	if os.path.isdir(input_filepaths):
 		sr_logger.info("Directory Path: " + str(input_filepaths))
 		all_files = os.listdir(input_filepaths)
@@ -41,9 +47,9 @@ def super_resolution_upscale_video(argv):
 		sr_logger.info("File Path: " + str(input_filepaths))
 		input_filepaths: list = [input_filepaths]
 
-	output_dir = args.save_path
+	output_dir = parsed_args.save_path
 
-	program_path: str = 'ffmpeg'
+	program_path: str = 'ffmpeg' # TODO: remove when using ffmpeg binding only.
 
 	if which('ffmpeg') is None:
 		raise Exception("ffmpeg not found")
@@ -54,13 +60,21 @@ def super_resolution_upscale_video(argv):
 		video_full_path = str(os.path.abspath(video_path))
 		output_final_video = os.path.join(output_dir, os.path.basename(input_file_path))
 
-		# Attempt to Extract video info
+		# Create directory
+		output_file_dir_path  = os.path.dirname(output_final_video)
+		if not os.path.exists(output_file_dir_path):
+			os.mkdir(output_file_dir_path)
+
+		sr_logger.debug("Output File Path %s", output_final_video)
+
+
+		# Attempt to Extract video meta information
 		try:
 			vid = ffmpeg.probe(filename=video_full_path)
 			streams = vid['streams']
 
 			video_streams = list(filter(lambda x: x['codec_type'] == 'video', streams))
-			video_stream =  video_streams[0]
+			video_stream = video_streams[0]
 
 			audio_streams = list(filter(lambda x: x['codec_type'] == 'audio', streams))
 			audio_stream = audio_streams[0] if len(audio_streams) > 0 else None
@@ -68,24 +82,25 @@ def super_resolution_upscale_video(argv):
 			fps_rate = eval(video_stream['avg_frame_rate'])
 			video_width = video_stream['width']
 			video_height = video_stream['height']
+			source_r_frame_rate = video_stream['r_frame_rate']
+			print(video_stream)
+			frame_count = 0
 
 			source_extract_fps_rate = eval(video_stream['r_frame_rate'])
 		except Exception:
 			sr_logger.error("Failed to Extract Video Meta from %s | Skipping File", video_full_path)
 			continue
-		
-		# Extract Video Frames
-		out, _ = (ffmpeg.input(video_full_path).output('pipe:', format='rawvideo', pix_fmt='rgb24').run(capture_stdout=True))
-		video_data = np.frombuffer(out, np.uint8).reshape([-1, video_height, video_width, 3])
-		del out
-		out = None
-		
-		print(video_data.shape)
 
-		# 
+		# Extract Video Frames TODO: make async 
+		input_frame_process, _ = (
+			ffmpeg.input(video_full_path, r=source_r_frame_rate).output('pipe:', format='rawvideo', pix_fmt='rgb24', vframes=360).run(capture_stdout=True))
+		video_data = np.frombuffer(input_frame_process, np.uint8).reshape([-1, video_height, video_width, 3])
+
+		#
 		try:
 			sr_logger.info("Starting %s", video_full_path)
 			with tempfile.TemporaryDirectory() as tmpdirname:
+
 				sr_logger.info('Created temporary directory %s', str(tmpdirname))
 
 				source_image_path = os.path.join(tmpdirname, 'source')
@@ -96,6 +111,7 @@ def super_resolution_upscale_video(argv):
 				sr_logger.debug(upscale_image_path)
 				os.mkdir(upscale_image_path)
 
+
 				# Save images to tmp directory
 				if video_data is None:
 					sr_logger.debug("Prepare to Extract Video Frames to %s", source_image_path)
@@ -104,7 +120,7 @@ def super_resolution_upscale_video(argv):
 					source_extract_fps_rate = str.format("fps={0}", fps_rate)
 					video_image_extract_command.extend(
 						[program_path, '-i', video_full_path, '-vf', source_extract_fps_rate,
-						source_extract_destination_pattern])
+						 source_extract_destination_pattern])
 					result = subprocess.call(video_image_extract_command, stdout=subprocess.PIPE)
 
 					# Check if extraction of files successed
@@ -118,32 +134,47 @@ def super_resolution_upscale_video(argv):
 				upscale_commands = []
 				upscale_commands.extend(['--input-file', "video_data"])
 				upscale_commands.extend(['--save-output', upscale_image_path])
-				upscale_commands.extend(['--model', args.model_filepath])
-
-				super_resolution_upscale(upscale_commands, np=video_data)
-
-				# Create directory
-				os.mkdir(os.path.dirname(output_final_video))
+				upscale_commands.extend(['--model', parsed_args.model_filepath])
 
 				# Convert the upscale.
 				upscale_extract_destination_pattern = os.path.join(upscale_image_path, frame_pattern)
-				output_upscale_video_path = os.path.join(tmpdirname, 'upscale_video.mp4')
+				output_upscale_video_path = os.path.join("./", 'upscale_video.mp4')
 				create_video_command = []
-				source_extract_fps_rate = "30"
+				#source_extract_fps_rate = "30"
 
-				#TODO: fix frame rate, being out of sync.
-				# '24/1.001'
-				create_video_command.extend(
-					[program_path, '-start_number', '1', '-framerate', '24', '-i', upscale_extract_destination_pattern,
-					 '-c:v', 'libx265', '-crf', '15', '-r', source_extract_fps_rate, '-pix_fmt', 'yuv420p',
-					 output_upscale_video_path])
-				result = subprocess.call(create_video_command, stdout=subprocess.PIPE)
-				if result != 0:
-					sr_logger.error("Failed to construct video from the upscaled images | Skipping file:")
-					continue
+				sr_logger.info(output_upscale_video_path)
+				output_process = ffmpeg.input('pipe:', format='rawvideo', pix_fmt='rgb24', s=f"{video_width * 2}x{video_height * 2}",  r=source_r_frame_rate ) \
+				.output(output_upscale_video_path, pix_fmt= 'yuv420p', vcodec=parsed_args.codec, crf=10, preset='slower', r=source_r_frame_rate) \
+				.overwrite_output() \
+				.run_async(pipe_stdin=True)
 
-				# Extract Audio # TODO: pipe 
-				extract_audio = False
+				def upscale_callback(arguments):
+					try:
+						#
+						frame_index, upscale_image, new_cropped_size, full_output_path = arguments
+					
+						# Crop image final size image.
+						upscale_image : Image = upscale_image.crop(new_cropped_size)
+
+						# Convert to correct data format.
+						pixel_data  = np.array(upscale_image).reshape([video_width * 2, video_height * 2, 3]).astype(np.uint8)
+
+						# 
+						#buffer, err =
+						output_process.stdin.write(pixel_data.tobytes())
+
+					except Exception as excep:
+						sr_logger.error(excep)
+
+				super_resolution_upscale(upscale_commands, np=video_data, upscale_callback=upscale_callback)
+
+
+
+				output_process.stdin.close()
+				output_process.wait()
+
+				# Extract Audio # TODO: pipe
+				is_audio_extracted = False
 				if audio_stream is not None:
 					audio_filename = str.format("video_audio.{}", audio_stream['codec_name'])
 					output_audio = os.path.join(tmpdirname, audio_filename)
@@ -151,29 +182,31 @@ def super_resolution_upscale_video(argv):
 					extract_audio_args.extend(['-i', video_path, '-vn', '-acodec', 'copy', output_audio])
 					result = subprocess.call(extract_audio_args, stdout=subprocess.PIPE)
 					if result == 0:
-						extract_audio = True
+						is_audio_extracted = True
 
-				if extract_audio: # TODO: pipe 
-					# Merge upscale and Audio. 
+				if is_audio_extracted:  # TODO: pipe
+					# Merge upscale and Audio.
 					merge_video_audio_args = [program_path]
 					merge_video_audio_args.extend(
-						['-i', output_upscale_video_path, '-i', output_audio, '-map', '0:v', '-map', '1:a', '-c:v', 'copy',
+						['-i', output_upscale_video_path, '-i', output_audio, '-map', '0:v', '-map', '1:a', '-c:v',
+						 'copy',
 						 '-shortest',
 						 output_final_video])
 					result = subprocess.call(merge_video_audio_args, stdout=subprocess.PIPE)
 
-					# 
+					#
 					if result != 0:
 						sr_logger.error("Failed to merge video and audio together | Skipping file")
 						continue
 				else:
 					sr_logger.info("No Audio Track, transfering file directly")
 					shutil.move(output_upscale_video_path, output_final_video)
-					
+
+				# CLEANUP Section
 				# Remove tmp original and upscaled files.
-				os.rmdir(source_image_path)
 				os.remove(output_audio)
-				os.rmdir(upscale_image_path)
+				os.remove(output_upscale_video_path)
+
 		except Exception as ex:
 			sr_logger.error("Error occurred during up-scaling of %s", video_full_path)
 			sr_logger.error(ex)
@@ -188,3 +221,17 @@ if __name__ == '__main__':
 		sr_logger.error(ex)
 		print(ex)
 		traceback.print_exc()
+
+
+
+
+# TODO: fix frame rate, being out of sync.
+# '24/1.001'
+#create_video_command.extend(
+#	[program_path, '-start_number', '1', '-framerate', '24', '-i', upscale_extract_destination_pattern,
+#	 '-c:v', 'libx265', '-crf', '15', '-r', source_extract_fps_rate, '-pix_fmt', 'yuv420p',
+#	 output_upscale_video_path])
+#result = subprocess.call(create_video_command, stdout=subprocess.PIPE)
+#if result != 0:
+#	sr_logger.error("Failed to construct video from the upscaled images | Skipping file:")
+#	continue
